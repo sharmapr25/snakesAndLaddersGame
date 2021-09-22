b7a0dea3a99fb309c51bfe6a5617a2eafbedd15b
preeti@coffeebeans.io <preetisharma@preetis-MacBook-Pro.local>
2021-09-22 15:32:14 +0530

    Created normal and crooked even type dices and written tests for that

--- src/Dice.js
+++ src/Dice.js
@@ -1,8 +1,18 @@
 class Dice{
+  constructor(numbers=[]){
+    this.numbers = numbers;
+  }
 
   roll(){
-    return Math.floor(Math.random()*6+1);
+    const index = Math.floor(Math.random() * this.numbers.length);
+    return this.numbers[index];
   }
 }
 
-module.exports = Dice;
+const dices = {
+  CROOKED_EVEN: new Dice([2,4,6]),
+  NORMAL: new Dice([1,2,3,4,5,6])
+}
+
+
+module.exports = dices;
--- test/dice.test.js
+++ test/dice.test.js
@@ -1,12 +1,19 @@
-const Dice = require("../src/Dice");
+const dices = require("../src/Dice");
 
 describe('roll', () => {
   it('should return any number between 1 to 6 when roll a normal dice', () => {
-    const dice = new Dice();
-    const number = dice.roll();
+    const number = dices.NORMAL.roll();
 
-    expect(number <= 6).toBeTruthy();
 
   });
+
+  it("should return any even number between 1 to 6 when roll a crooked even dice", () => {
+    const number = dices.CROOKED_EVEN.roll();
+
+    expect(number <= 6).toBeTruthy();
+    expect(number%2==0).toBeTruthy();
+  });
+
+
 
 });
