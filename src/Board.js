da9d1b2f56efb53730fe2161b0c1890d62cb8b55
preeti@coffeebeans.io <preetisharma@preetis-MacBook-Pro.local>
2021-09-22 14:50:55 +0530

    Added tests for board where snake bite a player

--- src/Board.js
+++ src/Board.js
@@ -3,10 +3,23 @@ const Snake = require("./Snake");
 class Board {
   constructor(size) {
     this.size = size;
+    this.snakes = [];
+  }
+
+  addSnake(snakePosition, length) {
+    this.snakes.push(new Snake(snakePosition, length));
+  }
+
+  getAnySnakeOnPosition(playerPosition){
+    return this.snakes.find(snake => snake.isOnSamePosition(playerPosition));
   }
 
   movePlayer(player, position) {
     player.position = player.position + position;
+    const snake = this.getAnySnakeOnPosition(player.position);
+    if (snake) {
+      snake.bite(player);
+    }
   }
 }
 
--- test/board.test.js
+++ test/board.test.js
@@ -2,19 +2,35 @@ const Board = require("../src/Board");
 const Player = require("../src/Player");
 
 describe('movePlayer', () => {
-  it('should move player to 3rd position when roll a dice and number is 3', () => {
+  it("should move player to 3rd position when roll a dice and number is 3", () => {
     const board = new Board(100);
-    const alice = new Player('Alice');
+    const alice = new Player("Alice");
     board.movePlayer(alice, 3);
 
     expect(alice.position).toEqual(3);
   });
 
-    it("should move player to 10th position when roll a dice and number is 6 and player position is 4", () => {
-      const board = new Board(100);
-      const alice = new Player("Alice", 4);
-      board.movePlayer(alice, 6);
+  it("should move player to 10th position when roll a dice and number is 6 and player position is 4", () => {
+    const board = new Board(100);
+    const alice = new Player("Alice", 4);
+    board.movePlayer(alice, 6);
 
-      expect(alice.position).toEqual(10);
-    });
+    expect(alice.position).toEqual(10);
+  });
+
+  it("should move player to 7 for given dice number is 4 when player current position is 10 and there is a snake at position 14", () => {
+    const board = new Board(100);
+    const alice = new Player("Alice", 10);
+    board.addSnake(14, 7);
+    board.movePlayer(alice, 4);
+    expect(alice.position).toEqual(7);
+  });
+
+  it("should move player to 10 for given dice number is 4 when player current position is 6 and there is a snake at position 14", () => {
+    const board = new Board(100);
+    const alice = new Player("Alice", 6);
+    board.addSnake(14, 7);
+    board.movePlayer(alice, 4);
+    expect(alice.position).toEqual(10);
+  });
 });
