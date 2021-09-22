297ad8b084e9bebf7b56b0714b95821b1edbfb28
preeti@coffeebeans.io <preetisharma@preetis-MacBook-Pro.local>
2021-09-22 16:06:06 +0530

    Added test for rolling dice twice in game

--- test/game.test.js
+++ test/game.test.js
@@ -28,4 +28,25 @@ describe('rollTheDice', () => {
 
     expect(player.position).toBe(10);
   });
+
+  it("should move player to the same position as 7 when roll the dice twice with acquired numbers 3 and 4 and there is a snake at 14th position", () => {
+    const board = new Board(100);
+    board.addSnake(14, 7);
+    const player = new Player("Alice", 7);
+    const mockedDice = dices.NORMAL;
+    const game = new Game(board, player, mockedDice);
+
+    //mock dice to return number 3
+    mockedDice.roll = jest.fn(() => 3);
+
+    game.rollTheDice();
+
+    //mock dice to return number 3
+    mockedDice.roll = jest.fn(() => 4);
+
+    game.rollTheDice();
+
+    expect(player.position).toBe(7);
+  });
+
 });
