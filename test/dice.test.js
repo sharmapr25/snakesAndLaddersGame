168a1a0f1e899e3e7ecd194673dbd1398e561388
preeti@coffeebeans.io <preetisharma@preetis-MacBook-Pro.local>
2021-09-22 15:32:50 +0530

    Added missing expect to normal dice test in dice.test.js

--- test/dice.test.js
+++ test/dice.test.js
@@ -3,8 +3,7 @@ const dices = require("../src/Dice");
 describe('roll', () => {
   it('should return any number between 1 to 6 when roll a normal dice', () => {
     const number = dices.NORMAL.roll();
-
-
+    expect(number <= 6).toBeTruthy();
   });
 
   it("should return any even number between 1 to 6 when roll a crooked even dice", () => {
